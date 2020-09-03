63ec2339a4cddb7310be8f53f7488a59145ad876
cjsantiago <cjsantiago@gatech.edu>
2020-09-02 23:34:30 -0400

    finish class

--- poisson_ar.py
+++ poisson_ar.py
@@ -1,49 +1,55 @@
+"""Module for Generalized Poisson TimeSeries Model"""
 from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
 from statsmodels.discrete.discrete_model import GeneralizedPoisson
-from statsmodels.tsa.ar_model import AutoReg
-from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from statsmodels.tools.tools import add_constant
+from statsmodels.tsa.ar_model import AutoReg
 
 
 class PoissonTimeSeries(BaseEstimator, RegressorMixin):
-
-    def __init__(self):
+    """Estimator for Generalized Poisson model with autoregressive residuals."""
+    def __init__(self, lags=1):
+        """Constructor"""
+        self.X_ = None
+        self.y_ = None
+        self.lags_ = lags
+        self.intercept_ = None
+        self.coef_ = None
+        self.alpha_ = None
         self.poisson_fit = None
         self.autoreg_fit = None
 
     def _fit_poisson(self, X, y):
+        """Fit a Poisson model."""
         mod = GeneralizedPoisson(endog=y, exog=X)
-        return mod.fit()
+        self.poisson_fit = mod.fit()
+        params = self.poisson_fit.params
+        self.intercept_ = params[0]
+        self.alpha_ = params[-1]
+        self.coef_ = params[1:-1]
 
-    def _fit_autoreg(self, endog, lags=1):
-        mod = AutoReg(endog, lags)
-        return mod.fit()
+    def _fit_autoreg(self, endog):
+        """Fit an AR model."""
+        mod = AutoReg(endog, self.lags_)
+        self.autoreg_fit = mod.fit()
 
-    def fit(self, X, y, **kwargs):
+    def fit(self, X, y):
+        """Fit the estimator."""
         X = add_constant(X)
         X, y = check_X_y(X, y)
         self.X_ = X
         self.y_ = y
 
-        self.poisson_fit = self._fit_poisson(X, y)
+        self._fit_poisson(X, y)
         poisson_resid = self.poisson_fit.resid
-        self.autoreg_fit = self._fit_autoreg(poisson_resid)
+        self._fit_autoreg(poisson_resid)
         return self
 
     def predict(self, X):
+        """Predict the response."""
         check_is_fitted(self)
         X = add_constant(X)
         X = check_array(X)
         poisson_pred = self.poisson_fit.predict(X)
         autoreg_pred = self.autoreg_fit.forecast(len(X))
         return poisson_pred + autoreg_pred
-
-if __name__ == '__main__':
-    from sklearn.datasets import load_diabetes
-    from sklearn.model_selection import train_test_split
-
-    X, y = load_diabetes(return_X_y=True)
-    x_train, x_test, y_train, y_test = train_test_split(X, y)
-    mod = PoissonTimeSeries()
-    fit = mod.fit(x_train, y_train)
-    mod.predict(x_test)
---
+++ test.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from poisson_ar import PoissonTimeSeries
+
+def main():
+    X, y = load_diabetes(return_X_y=True)
+    x_train, x_test, y_train, y_test = train_test_split(X, y)
+    mod = PoissonTimeSeries()
+    fit = mod.fit(x_train, y_train)
+    print(mod.score(x_train, y_train))
+    print(mod.coef_)
+    print(mod.intercept_)
+    print(mod.alpha_)
+    mod.predict(x_test)
+
+
+if __name__ == '__main__':
+    main()
